33c78fdbf0fd548fb831c3d52486ab8ef5f1eee2
malixian <malixian@ict.ac.cn>
2025-04-09 17:48:38 +0800

    add pass for hip and sycl

--- paddle/cinn/optim/optimize.cc
+++ paddle/cinn/optim/optimize.cc
@@ -108,7 +108,7 @@ ir::LoweredFunc Optimize(ir::LoweredFunc fn,
 #endif
       },
       [&](std::variant<common::HygonDCUArchHIP, common::HygonDCUArchSYCL>) {
-#ifdef CINN_WITH_HIP
+#if defined(CINN_WITH_HIP) || defined(CINN_WITH_SYCL)
         ir::SetCudaAxisInfo(copied);
         if (remove_gpu_for_loops) {
           VLOG(4) << "Before removing GPU for loops:\n" << copied;
@@ -122,10 +122,14 @@ ir::LoweredFunc Optimize(ir::LoweredFunc fn,
         blk_pass_manager.AddPass(CreateCudaSyncThreadsDropIfThenElsePass());
         blk_pass_manager.Run(copied->body_block);
         VLOG(10) << "After Optimize CudaSyncThreadsDropIfThenElse:" << copied;
+        FuncPassManager func_pass_manager;
+        VLOG(10) << "Before Optimize TransBufferWithDynamicShape:" << copied;
+        func_pass_manager.AddPass(CreateTransBufferWithDynamicShapePass());
+        func_pass_manager.Run(copied);
+        VLOG(10) << "After Optimize TransBufferWithDynamicShape:" << copied;
 #endif
       },
-      [](auto) {});
-
+      [&](std::variant<common::UnknownArch, common::X86Arch, common::ARMArch>) {});
   SimplifyUnitBlock(&copied->body);
   VLOG(4) << "After SimplifyUnitBlock:" << copied;
 
@@ -157,8 +161,13 @@ ir::LoweredFunc Optimize(ir::LoweredFunc fn,
         func_pass_manager.Run(copied);
         VLOG(4) << "After Optimize RearrangeLoadInstruction:" << copied;
       },
-      [](auto) {});
-
+      [&](std::variant<common::HygonDCUArchHIP, common::HygonDCUArchSYCL>) {
+        FuncPassManager func_pass_manager;
+        func_pass_manager.AddPass(CreateRearrangeLoadInstructionPass());
+        func_pass_manager.Run(copied);
+        VLOG(4) << "After Optimize RearrangeLoadInstruction:" << copied;
+      },
+      [&](std::variant<common::UnknownArch, common::X86Arch, common::ARMArch>) {});
   VectorizeForTrans(&copied->body);
   VLOG(10) << "After Optimize vectorize" << copied;
 
